865279463cd2c865a3f5416971e5aadea25fb906
NakOh <kk0704@naver.com>
2015-02-05 23:53:07 +0900

    First Study

---
+++ Hello.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+    <meta http-equiv="Content=Type" content = "text/html; charset=euc-kr" />
+    <title>안녕~~</title>
+
+</head>
+<body>
+<?
+echo "<p>안녕하세요</p>";
+?>
+
+</body>
+</html>
